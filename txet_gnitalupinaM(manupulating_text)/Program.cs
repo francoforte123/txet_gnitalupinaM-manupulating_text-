10cd1626a83a0af085953a3faf854f4abff69553
Emmanuele La Duca <laducaemanuele20@gmail.com>
2023-06-20 16:51:19 +0200

    Aggiungere i file di progetto.

---
+++ txet_gnitalupinaM(manupulating_text)/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("insert the text= ");
+String text= Console.ReadLine();
+
+Console.WriteLine("\n");
+Console.WriteLine("you have insert this text= " + text);
+
+String resultText = " ";
+
+for(int i = text.Length; i > 0; i--){
+    resultText += text[i-1];
+}
+
+Console.WriteLine("the result text is= " + resultText);
+
+
